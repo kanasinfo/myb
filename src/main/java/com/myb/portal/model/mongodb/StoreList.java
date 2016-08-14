438c107c099539499cd44bd7ecc75a38f7d20eb9
wangzx <zhen_xing_wang@163.com>
2016-08-14 21:38:44 +0800

    asdfsf

---
+++ src/main/java/com/myb/portal/model/mongodb/StoreList.java
@@ -0,0 +1,26 @@
+package com.myb.portal.model.mongodb;
+
+public class StoreList {
+	private String storeId;
+	private String storeName;
+	private String url;
+	public String getStoreId() {
+		return storeId;
+	}
+	public void setStoreId(String storeId) {
+		this.storeId = storeId;
+	}
+	public String getStoreName() {
+		return storeName;
+	}
+	public void setStoreName(String storeName) {
+		this.storeName = storeName;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	
+}
