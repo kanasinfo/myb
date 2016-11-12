0f33c2f1fe64ac79ee6d3cb6d26fb1851731efa8
wangzx <zhen_xing_wang@163.com>
2016-11-12 13:29:35 +0800

    sadf

--- src/main/java/com/myb/portal/util/InitListener.java
+++ src/main/java/com/myb/portal/util/InitListener.java
@@ -2,18 +2,31 @@ package com.myb.portal.util;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
 
 /**
  * Application Lifecycle Listener implementation class InitListener
  *
  */
-public class InitListener implements ServletContextListener {
+public class InitListener implements HttpSessionListener,ServletContextListener {
 	public void contextDestroyed(ServletContextEvent arg0) {
 
 	}
 
 	public void contextInitialized(ServletContextEvent arg0) {
 		arg0.getServletContext().setAttribute("ctx", arg0.getServletContext().getContextPath());
+	}
+
+	@Override
+	public void sessionCreated(HttpSessionEvent se) {
+		se.getSession().setAttribute("ctx", se.getSession().getServletContext().getContextPath());
+	}
+
+	@Override
+	public void sessionDestroyed(HttpSessionEvent se) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
